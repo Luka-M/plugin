9a5ea5f0a8f2f898ea72dfa0d729a893601cdaa5
Luka Mijatovic <luka@kodbiro.com>
2020-11-26 21:10:18 +0100

    Update

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3002
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+module.exports = function startServer(port) {
+	const express = require('express')
+	const app = express()
+	const port = port
+
+	app.get('/', (req, res) => {
+	  res.send('Hello World!')
+	})
+
+	app.listen(port, () => {
+	  console.log(`Example app listening at http://localhost:${port}`)
+	})	
+}
